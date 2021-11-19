ee042a7492821887b24809a9e457e9d818ac637b
Mitar <m.mitrovic@itsinbox.com>
2021-11-19 16:41:22 +0100

    Initial Commit

---
+++ Client/ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientApp.swift
+//  Client
+//
+//  Created by vesko on 19.11.21..
+//
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
